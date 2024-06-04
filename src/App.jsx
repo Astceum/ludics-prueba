b0027cdf8bc333364fb36e2095349c53b394f0eb
claudio <contacto.claudio93@gmail.com>
2024-06-04 18:10:52 -0400

    first name

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <div id="video-estrellas-container">
+                <video id="video-estrellas" src="https://res.cloudinary.com/di5yg1rzm/video/upload/v1717449915/home-local/fondo-estrellado_2_fntjdr.mp4" type="video/mp4" loop autoPlay muted></video>
+      </div>
+    </>
+  )
+}
+
+export default App
